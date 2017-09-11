0f3868864426ffa52e5eea34924315d803499887
zxsean <zxsean@126.com>
2017-09-11 12:09:31 +0800

    [*]提交分支

--- Plugins/Wox.Plugin.Express/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Wox.Plugin.Express")]
-[assembly: Guid("d4b98892-9dc7-448b-88cc-64cfa8f22ba4")]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Wox.Express
-WoxPlugin.Express
